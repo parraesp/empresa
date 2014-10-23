12f8887de8e51c641a33342211952d0027510848
parraesp <alc.htc.user@gmail.com>
2014-10-23 12:57:06 +0200

    new class

---
+++ Empresa.py
@@ -0,0 +1,6 @@
+__author__ = 'alberto'
+def __init__(self, nombre_empresa, cif, razon_social):
+        self.nombre_empresa = nombre_empresa
+        self.cif = cif
+        self.razon_social = razon_social
+        self.lista_departamentos = []
